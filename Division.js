73b6189dd0208896847670cf965c5f8c2c8a2c49
ArnavSingh77 <72737311+ArnavSingh77@users.noreply.github.com>
2020-12-25 17:42:51 +0530

    Update Division.js

--- Division.js
+++ Division.js
@@ -4,7 +4,6 @@ class Division{
           isStatic: true
         }
         this.body= Bodies.rectangle(x,y,width,height,options);
-        this.color="white";
         this.width=width;
         this.height=height;
         World.add(world,this.body);
@@ -12,7 +11,7 @@ class Division{
     display(){
         var pos = this.body.position;
         rectMode(CENTER);
-        fill(this.color);
+        fill("white");
         rect(pos.x,pos.y,this.width,this.height);
     }
 };
